0aaed21cc809d334237de60e49b226d9b9fa3e30
nitakumari <nitakumari94@gmail.com>
2021-02-25 23:53:58 +0530

    This is test

--- src/test/java/Qaclickacademy/mavanProject/AppiumTest.java
+++ src/test/java/Qaclickacademy/mavanProject/AppiumTest.java
@@ -13,4 +13,14 @@ public class AppiumTest {
 	public void demotest6() {
 		System.out.println("demotest6");
 	}
+	
+	@Test
+	public void demotest7() {
+		System.out.println("demotest8");
+	}
+	
+	@Test
+	public void demotest8() {
+		System.out.println("demotest8");
+	}
 }
--- src/test/java/Qaclickacademy/mavanProject/RestApiTest.java
+++ src/test/java/Qaclickacademy/mavanProject/RestApiTest.java
@@ -13,4 +13,14 @@ public class RestApiTest {
 	public void demotest4() {
 		System.out.println("demotest4");
 	}
+	
+	@Test
+	public void demotest9() {
+		System.out.println("demotest9");
+	}
+	
+	@Test
+	public void demotest10() {
+		System.out.println("demotest10");
+	}
 }
